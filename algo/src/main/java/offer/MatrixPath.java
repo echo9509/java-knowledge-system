754668fe0dc3a4194c277a113880d39f8a32b06c
sh <18852953917@163.com>
2022-11-27 10:27:39 +0800

    矩阵路径和下一个排列

---
+++ algo/src/main/java/offer/MatrixPath.java
@@ -0,0 +1,60 @@
+package offer;
+
+/**
+ * 矩阵中的路径
+ * 给定一个m x n 二维字符网格board 和一个字符串单词word 。如果word 存在于网格中，返回 true ；否则，返回 false 。
+ * <p>
+ * 单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母不允许被重复使用。
+ *
+ * @author sh
+ */
+public class MatrixPath {
+
+    public static boolean exist(char[][] board, String word) {
+        int h = board.length, w = board[0].length;
+        boolean[][] visited = new boolean[h][w];
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                if (check(board, visited, i, j, word, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static boolean check(char[][] board, boolean[][] visited, int i, int j, String word, int k) {
+        if (board[i][j] != word.charAt(k)) {
+            return false;
+        }
+        if (k == word.length() - 1) {
+            return true;
+        }
+        visited[j][j] = true;
+        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        boolean result = false;
+        for (int[] dir : directions) {
+            int newI = i + dir[0], newJ = j + dir[1];
+            if (newI >= 0 && newI < board.length && newJ >= 0 && newJ < board[0].length) {
+                if (!visited[newI][newJ]) {
+                    if (check(board, visited, newI, newJ, word, k + 1)) {
+                        result = true;
+                        break;
+                    }
+                }
+            }
+        }
+        visited[i][j] = false;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        char[][] params = new char[][]{
+                {'A', 'B', 'C', 'E'},
+                {'S', 'F', 'C', 'S'},
+                {'A', 'D', 'E', 'E'}
+        };
+        System.out.println(exist(params, "ABCCED"));
+    }
+
+}
